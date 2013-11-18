80d522ea0a906e56989121fad034f0274a7a73f2
cat.bakun <cat.bakun@facebook.com>
2013-11-18 00:00:23 -0300

    Initial commit.

---
+++ app.js
@@ -0,0 +1,73 @@
+var
+	gameport 		= process.env.PORT || 4005,
+
+	io				= require('socket.io'),
+	express			= require('express'),
+	UUID			= require('node-uuid'),
+
+	verbose			= false,
+	http			= require('http'),
+	app				= express(),
+	server			= http.createServer(app);
+	
+
+server.listen(gameport)
+
+app.get( '/', function( req, res ){
+	console.log('trying to load %s', __dirname + '/index.html');
+	res.sendfile( '/index.html' , { root:__dirname });
+});
+
+
+app.get( '/*' , function( req, res, next ) {
+
+		//This is the current file they have requested
+	var file = req.params[0];
+
+		//For debugging, we can track what files are requested.
+	if(verbose) console.log('\t :: Express :: file requested : ' + file);
+
+		//Send the requesting client the file.
+	res.sendfile( __dirname + '/' + file );
+
+}); //app.get *
+
+
+var sio = io.listen(server);
+var clients = {length: 0};
+	//Configure the socket.io connection settings.
+	//See http://socket.io/
+sio.configure(function (){
+
+	sio.set('log level', 0);
+
+	sio.set('authorization', function (handshakeData, callback) {
+	  callback(null, true); // error first callback style
+	});
+
+});
+
+
+
+sio.sockets.on('connection', function (client) {
+	console.log("Someone got connected: ");
+
+	client.UUID = UUID();
+	
+	clients[client.UUID] = client;
+	
+	if(clients.length > 0){
+		role = 'c' /** I'm not the fist client, so i'm not the host, I'm a client */
+	}else{
+		role = 'h' /** There is no other client in the game, so I'm the host */
+	}
+	
+	clients.length++;
+	
+	client.emit('onconnected', { id: client.userid } );
+	
+	/** i: init
+	 *  h: host */
+	client.emit('onmessage', ['ig',role])
+})
+
---
+++ client.js
@@ -0,0 +1,25 @@
+(function(){
+	
+	var socket = io.connect();
+	socket.on('connect', function(){
+		console.log('Socket connection established');
+	});
+	
+	socket.on('onconnected', function(data){
+		console.log("Player created: ", data)
+	})
+	
+	socket.on('onmessage', function(data){
+		var handler = data[0];
+		
+		switch( handler ){
+			case 'ig' : (function(settings){
+				console.log('Initializing game', settings)
+			})({
+				role : data[1]
+			})
+		}
+	});
+	
+	
+})();
